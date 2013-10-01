7a8d9324602170ab6f9111dd95d832bab13522ab
Bidon Pomoev <yoba123@yandex.ru>
2013-10-01 04:57:47 +0400

    geoip fix

--- flask-fingerprint.py
+++ flask-fingerprint.py
@@ -19,9 +19,9 @@ def send_file(filename):
 
 gi = None
 gi_org = None
-if os.path.exists('GeoLiteCity.dat') and os.path.exists('GeoIPOrg.dat'):
+if os.path.exists('GeoIPCity.dat') and os.path.exists('GeoIPOrg.dat'):
     import pygeoip
-    gi = pygeoip.GeoIP('GeoLiteCity.dat') # http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+    gi = pygeoip.GeoIP('GeoIPCity.dat') # http://rutracker.org/forum/viewtopic.php?t=4210999
     gi_org = pygeoip.GeoIP('GeoIPOrg.dat') # https://thepiratebay.sx/torrent/8521369/
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -30,10 +30,10 @@ def index():
         uid = request.json['user_name']
         raw = request.json.__repr__()
         acc_hash = hashlib.md5(request.json['mimetypes'].encode('utf-8') + request.json['fonts_all'].encode('utf-8') + request.json['plugins_all'].encode('utf-8')).hexdigest()
-        geoip = gi.record_by_addr(request.remote_addr)['country_name'] if gi else 'Not available'
+        geoip = gi.record_by_addr(request.remote_addr) if gi else 'Not available'
         geoip_org = gi_org.org_by_addr(request.remote_addr) if gi_org else 'Not available'
         inacc_hash = hashlib.md5(str(request.json['timezone']) + request.json['os'] + request.json['screen']).hexdigest()
-        return jsonify(result='Accurate hash: %s<br>Inaccurate hash: %s<br>Evercookie: %s<br>Country: %s<br>ISP: %s' % (acc_hash, inacc_hash, uid, geoip, geoip_org))
+        return jsonify(result='Accurate hash: %s<br>Inaccurate hash: %s<br>Evercookie: %s<br>GeoIP: %s<br>ISP: %s' % (acc_hash, inacc_hash, uid, geoip, geoip_org))
     return render_template("index.html")
 
 if __name__ == '__main__':
