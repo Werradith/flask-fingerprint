e87d0a0f082caa4d3ed6558d9fff89fa97c38100
Bidon Pomoev <yoba123@yandex.ru>
2013-10-03 22:08:36 +0400

    run.py

--- flask_fingerprint.py
+++ flask_fingerprint.py
@@ -39,11 +39,7 @@ def index():
         return jsonify(result='Browser hash: %s<br>System hash: %s<br>Evercookie: %s<br>GeoIP: %s<br>ISP: %s' % (acc_hash, inacc_hash, uid, geoip, geoip_org))
     return render_template("index.html")
 
-if __name__ == '__main__':
-    deps_js = Bundle('javascript/swfobject.min.js', 'javascript/jquery.min.js', 'javascript/jquery-ui-1.8.16.custom.min.js', 'javascript/jquery.json-2.3.min.js', 'javascript/jquery.flash.js', 'javascript/evercookie/evercookie.js', 'javascript/sha1.js')
-    fp_js = Bundle('javascript/plugindetect/plugindetect.js', 'javascript/fontdetect.js', 'javascript/fingerprint.js')
-    #css = Bundle('style.css', filters='yui_css', output='get/styles.css')
-    #assets_env.register('js_fingerprint_deps', deps_js)
-    #assets_env.register('js_fingerprint_fp', fp_js)
-    assets_env.register('js_fingerprint', Bundle(deps_js, fp_js, filters='yui_js', output='gen/fingerprint.js'))
-    app.run(debug=True, threaded=True, host='0.0.0.0')
+# On run
+deps_js = Bundle('javascript/swfobject.min.js', 'javascript/jquery.min.js', 'javascript/jquery-ui-1.8.16.custom.min.js', 'javascript/jquery.json-2.3.min.js', 'javascript/jquery.flash.js', 'javascript/evercookie/evercookie.js', 'javascript/sha1.js')
+fp_js = Bundle('javascript/plugindetect/plugindetect.js', 'javascript/fontdetect.js', 'javascript/fingerprint.js')
+assets_env.register('js_fingerprint', Bundle(deps_js, fp_js, filters='yui_js', output='gen/fingerprint.js'))
