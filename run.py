e06a70c4824a69524225cdc55c22b89907a161b6
Bidon Pomoev <yoba123@yandex.ru>
2013-10-03 23:47:41 +0400

    no message

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ DEBUG_PORT = 5000
 if __name__ == '__main__':
     import sys
     from flask_fingerprint import app
-    if sys.argv[1] == '-debug':
+    if '-debug' in sys.argv:
         app.run(port=DEBUG_PORT, debug=True, host=DEBUG_HOST)
     else:
-        app.run(port=PORT, debug=False, host=HOST)
+        app.run(port=PORT, debug=False, host=HOST, threaded=True)
