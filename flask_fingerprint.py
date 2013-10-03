537de413c508f49fae8dc882d06e0d2d0be8b424
Bidon Pomoev <yoba123@yandex.ru>
2013-10-03 22:26:51 +0400

    no message

--- flask_fingerprint.py
+++ flask_fingerprint.py
@@ -14,10 +14,10 @@ def inject_fujs():
 
 @app.route('/get-nickname')
 def get_nickname():
-    import string
-    import random
+    import string, random
     CANDIDATE_CHARS = string.ascii_letters+string.digits  # lowercase and uppercase le
     return u''.join(random.choice(CANDIDATE_CHARS) for _ in range(16))
+
 
 gi = None
 gi_org = None
@@ -25,17 +25,24 @@ if os.path.exists('GeoIPCity.dat') and os.path.exists('GeoIPOrg.dat'):
     import pygeoip
     gi = pygeoip.GeoIP('GeoIPCity.dat') # http://rutracker.org/forum/viewtopic.php?t=4210999
     gi_org = pygeoip.GeoIP('GeoIPOrg.dat') # https://thepiratebay.sx/torrent/8521369/
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         import hashlib
         uid = request.json['user_name']
-        raw = request.json.__repr__()
+        #raw = request.json.__repr__()
+
         acc_hash = hashlib.md5(request.json['mimetypes'].encode('utf-8') + request.json['fonts_all'].encode('utf-8') + request.json['plugins_all'].encode('utf-8')).hexdigest()
-        geoip = gi.record_by_addr(request.remote_addr) if gi else 'Not available'
-        geoip_org = gi_org.org_by_addr(request.remote_addr) if gi_org else 'Not available'
         inacc_hash = hashlib.md5(str(request.json['timezone']) + request.json['os'] + request.json['screen']).hexdigest()
+
+        client_ip = request.headers['X-Forwarded-For'] if request.headers.get('X-Forwarded-For') else request.remote_addr
+        geoip = gi.record_by_addr(client_ip) if gi else 'Not available'
+        geoip_org = gi_org.org_by_addr(client_ip) if gi_org else 'Not available'
+
         return jsonify(result='Browser hash: %s<br>System hash: %s<br>Evercookie: %s<br>GeoIP: %s<br>ISP: %s' % (acc_hash, inacc_hash, uid, geoip, geoip_org))
+
     return render_template("index.html")
 
 # On run
