79e99b55c68fa5cc0eb064429663629514ef7b29
Bidon Pomoev <yoba123@yandex.ru>
2013-10-07 03:00:20 +0400

    major improvements

--- flask_fingerprint.py
+++ flask_fingerprint.py
@@ -14,9 +14,11 @@ def inject_fujs():
 
 @app.route('/get-nickname')
 def get_nickname():
-    import string, random
-    CANDIDATE_CHARS = string.ascii_letters+string.digits  # lowercase and uppercase le
-    return u''.join(random.choice(CANDIDATE_CHARS) for _ in range(16))
+    import random
+    first_names = ['Lindsey', 'Clay', 'Leroy', 'Miles', 'Elmo', 'Shelby', 'Kurt', 'Lionel', 'Diego', 'Efrain', 'Rudy', 'Quintin', 'Elvin', 'Chester', 'Wilbur', 'Kenton', 'Jewell', 'Andrew', 'Dwayne', 'Kelvin']
+    last_names = ['Lanoue', 'Cage', 'Londono', 'Mincks', 'Esteban', 'Shambo', 'Kean', 'Lukas', 'Dallman', 'Elsey', 'Ridings', 'Quinones', 'Eisele', 'Chilson', 'Weakley', 'Kreps', 'Joo', 'Aguas', 'Dietz', 'Kantz']
+
+    return u'%s %s' % (random.choice(first_names), random.choice(last_names))
 
 
 gi = None
@@ -26,23 +28,24 @@ if os.path.exists('GeoIPCity.dat') and os.path.exists('GeoIPOrg.dat'):
     gi = pygeoip.GeoIP('GeoIPCity.dat') # http://rutracker.org/forum/viewtopic.php?t=4210999
     gi_org = pygeoip.GeoIP('GeoIPOrg.dat') # https://thepiratebay.sx/torrent/8521369/
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         import hashlib
-        uid = request.json['user_name']
-        #raw = request.json.__repr__()
+        fp_hashes = dict()
+        geoip_result = dict()
 
-        acc_hash = hashlib.md5(request.json['mimetypes'].encode('utf-8') + request.json['fonts_all'].encode('utf-8') + request.json['plugins_all'].encode('utf-8')).hexdigest()
-        inacc_hash = hashlib.md5(str(request.json['timezone']) + request.json['os'] + request.json['screen']).hexdigest()
-        nav_hash = request.json['navigator_hash']
+        fp_hashes['browser'] = hashlib.md5(request.json['mimetypes'].encode('utf-8') + request.json['fonts_all'].encode('utf-8') + request.json['navigator_hash'].encode('utf-8')).hexdigest()
+        fp_hashes['system'] = hashlib.md5(str(request.json['timezone']) + request.json['os'] + request.json['screen']).hexdigest()
+        #fp_hashes['navigator'] = request.json['navigator_hash']
+        fp_hashes['browser_hdrs'] = hashlib.md5(request.headers['Accept'] + request.headers['Accept-Language'] + request.headers['Accept-Encoding']).hexdigest()
 
-        client_ip = request.headers['X-Forwarded-For'] if request.headers.get('X-Forwarded-For') else request.remote_addr
-        geoip = gi.record_by_addr(client_ip) if gi else 'Not available'
-        geoip_org = gi_org.org_by_addr(client_ip) if gi_org else 'Not available'
 
-        return jsonify(result='Navigator hash: %s<br>Browser hash: %s<br>System hash: %s<br>Evercookie: %s<br>GeoIP: %s<br>ISP: %s' % (nav_hash, acc_hash, inacc_hash, uid, geoip, geoip_org))
+        geoip_result['client_ip'] = request.headers['X-Forwarded-For'] if request.headers.get('X-Forwarded-For') else request.remote_addr
+        geoip_result['city'] = gi.record_by_addr(geoip_result['client_ip']) if gi else None
+        geoip_result['org'] = gi_org.org_by_addr(geoip_result['client_ip']) if gi_org else None
+
+        return jsonify(result = render_template('fp_result.html', fp = request.json, fp_hashes = fp_hashes, gi = geoip_result))
 
     return render_template("index.html")
 
